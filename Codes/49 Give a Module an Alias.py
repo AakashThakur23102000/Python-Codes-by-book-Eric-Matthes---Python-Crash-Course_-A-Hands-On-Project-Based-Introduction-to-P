12c9f93af909128bfcfe8ca5ab22256fc7d408a5
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2022-08-17 16:04:45 +0530

    Signed-off-by: AakashThakur23102000 <aakashthakur20001972@gmail.com>

---
+++ Codes/49 Give a Module an Alias.py
@@ -0,0 +1,5 @@
+##Here module name is shortned as One while importing it to main function
+
+import ModuleOne as One
+
+One.pizzas("Large","ETC")
---
+++ Codes/50 Importing All Functions in a Module.py
@@ -0,0 +1,9 @@
+'''
+You can tell Python to import every function in a module by using the aster-
+isk (*) operator:
+
+'''
+
+from ModuleOne import *
+pizzas("Large","Aakash")
+cake("Large","Aakash")
